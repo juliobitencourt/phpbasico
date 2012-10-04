1f061a5a48a1e8142734266ceccbf70cb43fe369
Julio Bitencourt <julio@contagia.com.br>
2012-10-04 17:25:17 -0300

    Inclusão das classes em arquivo separado

--- aula1.php
+++ aula1.php
@@ -1,56 +1,15 @@
 <?php
 
-class veiculo{
-    public $nome_modelo = "";
-
-    public function nome($nome) {
-        $this->nome_modelo = $nome;
-    }
-
-}
-
-class carro extends veiculo
-{
-    public $velocidade_ini = 0;
-    public $velocidade_max = 220;
-   
-    function define_velocidade($velocidade){
-        if($velocidade > $this->velocidade_max)
-            echo "Limite de velocidade $this->nome_modelo: $this->velocidade_max KM/h";
-        else
-            $this->velocidade_ini = $velocidade;
-    }
-
-    function acelerar($velocidade=0)
-    {
-        $this->define_velocidade($velocidade);
-        echo '<br /> ' . $this->nome_modelo . ' esta acelerando a ' . $this->velocidade_ini . 'KM/h.<br />'; 
-    }
-}
-
-class picape extends carro 
-{
-    public $peso_carregado = 0;
-    public $peso_maximo = 500;
-    
-    function define_cacamba($peso) {
-        if ($peso > $this->peso_maximo)
-            echo "Limite de peso $this->nome_modelo: $this->peso_maximo Kg";
-        else
-            $this->peso_carregado = $peso;
-    }
-
-    public function transportar($peso=0)
-    {
-        $this->define_cacamba($peso);
-        echo '<br /> ' . $this->nome_modelo . ' esta carregando ' . $this->peso_carregado . ' kilos.'; 
-    }
-}
+include_once('classes.php');
 
 $veiculo = new picape;
 $veiculo->nome('S10');
-$veiculo->acelerar(350);
+$veiculo->acelerar(150);
+
 echo '<br/>';
 $veiculo->transportar(700);
 
-?>
+echo '<br/>';
+$veiculo2 = new carro;
+$veiculo2->nome('Siena');
+$veiculo2->acelerar(100);
---
+++ classes.php
@@ -0,0 +1,54 @@
+<?
+class veiculo{
+    public $nome_modelo = "";
+
+    public function nome($nome) {
+        $this->nome_modelo = $nome;
+    }
+
+}
+
+class carro extends veiculo
+{
+    public $velocidade_ini = 0;
+    public $velocidade_max = 220;
+   
+    function define_velocidade($velocidade){
+        if($velocidade > $this->velocidade_max)
+            echo "Limite de velocidade $this->nome_modelo: $this->velocidade_max KM/h";
+        else
+            $this->velocidade_ini = $velocidade;
+    }
+
+    public function acelerar($velocidade=0)
+    {
+        $this->define_velocidade($velocidade);
+        echo '<br /> ' . $this->nome_modelo . ' esta acelerando a ' . $this->velocidade_ini . 'KM/h.<br />'; 
+    }
+}
+
+class picape extends carro 
+{
+    public $peso_carregado = 0;
+    public $peso_maximo = 500;
+    
+    function define_cacamba($peso) {
+        if ($peso > $this->peso_maximo)
+            echo "Limite de peso $this->nome_modelo: $this->peso_maximo Kg";
+        else
+            $this->peso_carregado = $peso;
+    }
+
+    public function transportar($peso=0)
+    {
+        $this->define_cacamba($peso);
+        echo '<br /> ' . $this->nome_modelo . ' esta carregando ' . $this->peso_carregado . ' kilos.'; 
+    }
+
+    function acelerar($velocidade=0)
+    {
+        $this->define_velocidade($velocidade);
+        echo '<br /> ' . $this->nome_modelo . ' esta acelerando MUITO a ' . $this->velocidade_ini . 'KM/h.<br />'; 
+    }
+
+}
